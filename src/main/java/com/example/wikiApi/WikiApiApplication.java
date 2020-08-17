693fb675bd0668e37046d8f8fc5b4d68abc218da
Mateusz Jankowski <mateusz@jankesdev.pl>
2020-08-17 17:36:30 +0200

    Create project wikiApi

---
+++ src/main/java/com/example/wikiApi/WikiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wikiApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wikiApi/WikiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wikiApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
