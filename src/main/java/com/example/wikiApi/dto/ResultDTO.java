b1fc8153f62fdb330ad200e17d429d88bb6fbc44
Mateusz Jankowski <mateusz@jankesdev.pl>
2020-08-17 18:17:57 +0200

    Update project

--- src/main/java/com/example/wikiApi/dto/ResultDTO.java
+++ src/main/java/com/example/wikiApi/dto/ResultDTO.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class ResultDTO {
-    private QueryDTO queryDTO;
+    private QueryDTO query;
 }
--- src/main/java/com/example/wikiApi/service/WikiServiceImpl.java
+++ src/main/java/com/example/wikiApi/service/WikiServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.wikiApi.service;
 
 import com.example.wikiApi.dto.ResultDTO;
+import com.example.wikiApi.dto.SearchDTO;
+import com.example.wikiApi.exception.InternalServerException;
 import com.example.wikiApi.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -30,13 +33,22 @@ public class WikiServiceImpl implements WikiService {
 
         URI uri = URI.create(URL_API + "?action=query&list=search&format=json&srsearch=%22" + name + "%22&srlimit=10");
 
+        List<SearchDTO> searchDTOS;
+
         try {
             ResponseEntity<ResultDTO> responseEntity = restTemplate.getForEntity(uri, ResultDTO.class);
-
+            searchDTOS = responseEntity.getBody().getQuery().getSearch();
         } catch (RestClientException e) {
-            log.error("Server problem. Check api key");
+            log.error("Server problem with api, check api");
+            throw new InternalServerException("Server problem. Check api");
         }
 
-        return null;
+        if(searchDTOS.isEmpty()) {
+            //todo
+        }
+
+        SearchDTO searchDTO = searchDTOS.get(0);
+
+        return new ApiResponse();
     }
 }
